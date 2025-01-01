544ea312f6769ab72434be798fe6b59adee4b559
GET Academy <terje@kolderup.net>
2025-01-01 19:02:23 +0100

    Initial commit

---
+++ Chat/ChatClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    internal class ChatClient
+    {
+        private readonly string _name;
+        private readonly ChatServer _server;
+
+        public ChatClient(string name, ChatServer server)
+        {
+            _server = server;
+            _name = name;
+            _server.Register(this);
+        }
+
+        public void Say(string message)
+        {
+            _server.Broadcast(this, $"{_name} sier {message}.");
+        }
+
+        public void Receive(string message)
+        {
+            Console.WriteLine($"Logg fra {_name} sin klient - mottok: {message}");
+        }
+    }
+}
---
+++ Chat/Program.cs
@@ -0,0 +1,16 @@
+﻿using Chat;
+
+var server = new ChatServer();
+
+var per = new ChatClient("Per", server);
+var pål = new ChatClient("Pål", server);
+var espen = new ChatClient("Espen", server);
+
+per.Say("Hello");
+// Pål og Espen får beskjed "Per sier Hello"
+
+pål.Say("Hello");
+// Per og Espen får beskjed "Pål sier Hello"
+
+espen.Say("Hello");
+// Per og Pål får beskjed "Espen sier Hello"
