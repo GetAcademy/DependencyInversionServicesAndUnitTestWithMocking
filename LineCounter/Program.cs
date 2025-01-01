4bf08b991af808e191b615e5890251d3f5caf17b
GET Academy <terje@kolderup.net>
2025-01-01 19:07:53 +0100

    x

---
+++ LineCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using var reader = new StreamReader("vgsak.txt");
+var searchText = "e";
+var matchingLineCount = 0;
+var totalLineCount = 0;
+var line = reader.ReadLine();
+while (line != null)
+{
+    if (line.Contains(searchText))
+    {
+        matchingLineCount++;
+    }
+
+    totalLineCount++;
+    line = reader.ReadLine();
+}
+
+Console.WriteLine($"{matchingLineCount} av {totalLineCount} linjer inneholder {searchText}");
---
+++ LineCounter/Snippets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCounter
+{
+    internal class Snippets
+    {
+        public static void Run()
+        {
+            string url = null;
+            var webClient = new WebClient();
+            var fileStream = webClient.OpenRead(url);
+            var reader = new StreamReader(fileStream);
+        }
+    }
+}
